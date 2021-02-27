016b71efaba47facfafaaa2ceef80a1cab6b07ff
Sergio S. Quevedo <sergio.sanchez.quevedo@greysystems.eu>
2021-02-27 09:59:29 +0100

    Delete useless scripts folder

--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [[ -z $RELEASE ]]; then
-    echo "Error: undefined RELEASE environment variable."
-    exit 1
-fi
-
-docker build -t eu.greysystems/docker-maven-aws:1.0.0 .
